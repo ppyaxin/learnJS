5b02d9bec76d70f740149a65bb15bee2e4ea480e
panyaxin <pyx518@qq.com>
2021-05-28 16:58:13 +0800

    录音动画

--- learn/closure.js
+++ learn/closure.js
@@ -48,3 +48,52 @@ for (var i = 0; i < 3; i++) {
 data[0]();
 data[1]();
 data[2]();
+
+//闭包测试
+function fun(n, o) {
+    console.log(o);
+    return {
+        fun: function(m) {
+            return fun(m, n);
+        }
+    };
+}
+console.log("hhhhhh")
+var a = fun(0); // undefined
+a.fun(1); // 0
+a.fun(2); // 0
+a.fun(3); // 0
+console.log("hhhhhh")
+
+var b = fun(0).fun(1).fun(2).fun(3); // undefined 0 1 2
+var c = fun(0).fun(1); // undefined 0
+c.fun(2); // 1
+c.fun(3); // 1
+
+console.log("测试测试")
+var data = [];
+for(var i = 0; i < 3; i ++) {
+	data[i] = (function(i) {
+		return function() {
+			console.log(i);
+		}
+	}(i))
+}
+data[0]();
+data[1]();
+data[2]();
+
+
+console.log("测试测试")
+function fn1() {
+    for(var i = 0; i < 4; i ++) {
+        var tc=setTimeout(
+                function(i){
+                    console.log(i);
+                    console.log(tc);
+                    clearTimeout(tc)
+                }, 10 ,i);
+
+    }
+}
+fn1();
